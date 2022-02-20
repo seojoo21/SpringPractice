d025d758c2cdc7d0b22897a8869f4bfd2c012862
Seojoo <98722435+seojoo21@users.noreply.github.com>
2022-02-20 18:16:07 +0900

    BoardService 인터페이스
    - BoardService 메서드를 설계할 때 메서드 이름은 현실적인 로직의 이름을 붙이는 것이 관례이다.
    - 명백하게 반환해야 할 데이터가 있는 'select'를 해야 하는 메서드는 리턴 타입을 지정할 수 있다.
    - 특정한 게시물을 가져오는 get() 메서드와 전체 리스트를 구하는 getList()의 경우 처음부터 메서드의 리턴 타입을 결정해서 진행할 수 있다.

--- ex02/org.zerock.service/BoardService.java
+++ ex02/org.zerock.service/BoardService.java
@@ -3,6 +3,7 @@ package org.zerock.service;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardService {
 	
@@ -10,5 +11,10 @@ public interface BoardService {
 	public BoardVO get(Long bno);
 	public boolean modify(BoardVO board);
 	public boolean remove(Long bno);
-	public List<BoardVO> getList();
+	
+	// 게시물 페이징 처리를 위해 Criteria를 파라미터로 처리하도록 기존의 getList() 메서드를 getList(Criteria cri)로 수정
+	// public List<BoardVO> getList();
+	public List<BoardVO> getList(Criteria cri);
+	public int getTotal(Criteria cri);
 }
+
